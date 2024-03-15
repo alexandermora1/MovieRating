33b7ca2ec688e49713156ea0f190d45437fb4e7e
Alex <alexandermora@live.no>
2024-03-15 16:55:47 +0100

    Fix Folder nonsense, Update dotnet

---
+++ MovieRatingApi/Migrations/20240315155433_AddRatingToMovies.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieRatingApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRatingToMovies : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Rating",
+                table: "Movies",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ReleaseDate",
+                table: "Movies",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "ReleaseDate",
+                table: "Movies");
+        }
+    }
+}
